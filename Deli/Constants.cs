7d3d40a0add6ef75b603d26b81a527a716fd0302
Ash <ashe.null@protonmail.ch>
2020-11-16 11:03:51 -0600

    add git consts

--- Deli/Constants.cs
+++ Deli/Constants.cs
@@ -7,6 +7,11 @@
         public const string Guid = "nrgill28.deli";
         public const string Version = "1.0.0.0";
 
+        // Git
+        // These are set in the makefile, via the "macros" recipe
+        public const string GitBranch = "STUB_GIT_BRANCH";
+        public const string GitHash = "STUB_GIT_HASH";
+
         // Loader constants
         public const string ModExtension = "zip";
         public const string ModDirectory = "mods";
