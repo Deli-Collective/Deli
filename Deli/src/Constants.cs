8e7b97c2a78015af5652b74fac7022b1f564cdb5
Nathan Gill <nrgill28@gmail.com>
2020-11-27 14:37:36 -0500

    IEnumerables are better I guess

--- Deli/src/Constants.cs
+++ Deli/src/Constants.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Deli
@@ -16,8 +17,7 @@ namespace Deli
 		public const string GitHash = "MACRO_GIT_HASH";
 
 		// Loader constants
-		public static ReadOnlyCollection<string> ModExtensions { get; } =
-			new ReadOnlyCollection<string>(new []{"zip", "deli"});
+		public static IEnumerable<string> ModExtensions { get; } = new ReadOnlyCollection<string>(new []{"zip", "deli"});
 		public const string ModDirectory = "mods";
 		public const string ConfigDirectory = "mods/configs";
 		public const string ManifestFileName = "manifest.json";
