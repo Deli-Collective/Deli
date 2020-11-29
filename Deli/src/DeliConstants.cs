a84ba0c0815382d32bb6f747d12dc5db617d502c
Ash <ashe.null@protonmail.ch>
2020-11-29 17:33:39 -0600

    XML doc DeliConstants.cs

--- Deli/src/DeliConstants.cs
+++ Deli/src/DeliConstants.cs
@@ -4,23 +4,82 @@ using System.Collections.ObjectModel;
 namespace Deli
 {
 	// Constants beginning with MACRO are set via the "macros-precompile" recipe
+	/// <summary>
+	/// 	Constants that Deli uses.
+	/// </summary>
 	public static class DeliConstants
 	{
-		// GUID and Version
+		#region Meta
+
+		/// <summary>
+		/// 	The name of the runtime plugin and patcher plugin
+		/// </summary>
 		public const string Name = "Deli";
+
+		/// <summary>
+		/// 	The GUID of the runtime plugin
+		/// </summary>
 		public const string Guid = "nrgill28.deli";
+
+		/// <summary>
+		/// 	The version of the runtime plugin and patcher plugin
+		/// </summary>
 		public const string Version = "MACRO_VERSION";
 
-		// Git
+		#endregion
+
+		#region Git
+
+		/// <summary>
+		/// 	The result of the following command at compile-time:
+		/// 	<code>git describe --long --always --dirty</code>
+		/// </summary>
 		public const string GitDescribe = "MACRO_GIT_DESCRIBE";
+
+		/// <summary>
+		/// 	The result of the following command at compile-time:
+		/// 	<code>git rev-parse --abbrev-ref HEAD</code>
+		/// </summary>
 		public const string GitBranch = "MACRO_GIT_BRANCH";
+
+		/// <summary>
+		/// 	The result of the following command at compile-time:
+		/// 	<code>git rev-parse HEAD</code>
+		/// </summary>
 		public const string GitHash = "MACRO_GIT_HASH";
 
-		// Loader constants
-		public const string AssemblyLoaderName = "assembly";
-		public static IEnumerable<string> ModExtensions { get; } = new ReadOnlyCollection<string>(new []{"zip", "deli"});
+		#endregion
+
+		#region Filesystem
+
+		/// <summary>
+		/// 	The directory that contains mod files
+		/// </summary>
 		public const string ModDirectory = "mods";
+		/// <summary>
+		/// 	The file extensions that Deli will load as mods, given that it is in <seealso cref="ModDirectory"/>
+		/// </summary>
+		public static IEnumerable<string> ModExtensions { get; } = new ReadOnlyCollection<string>(new []{"zip", "deli"});
+
+		/// <summary>
+		/// 	The directory that contains mod configuration files
+		/// </summary>
 		public const string ConfigDirectory = "mods/configs";
+		/// <summary>
+		/// 	The file extensions that Deli will load as mod configurations, given that it is in <seealso cref="ConfigDirectory"/>
+		/// </summary>
+		public const string ConfigExtension = "cfg";
+
+		/// <summary>
+		/// 	The file name of the manifest file of a Deli mod
+		/// </summary>
 		public const string ManifestFileName = "manifest.json";
+
+		#endregion
+
+		/// <summary>
+		/// 	The name of the asset loader that loads managed assemblies (applies to patcher-time and runtime)
+		/// </summary>
+		public const string AssemblyLoaderName = "assembly";
 	}
 }
