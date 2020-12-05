ec7763eaa323c6b5a457db0168441b5bf8025069
Ash <ashe.null@protonmail.ch>
2020-12-05 17:27:51 -0600

    Use typeof instead of ToString()

--- Deli/src/Entities/Mods/Impl/LoggedResourceIO.cs
+++ Deli/src/Entities/Mods/Impl/LoggedResourceIO.cs
@@ -29,7 +29,7 @@ namespace Deli
 				throw new Exception($"Failed to retrieve asset at {path}", e);
 			}
 
-			_log.LogDebug($"Retrieved asset at {path}: {asset}");
+			_log.LogDebug($"Retrieved asset at {path}: {typeof(Option<T>)}");
 
 			return asset;
 		}
