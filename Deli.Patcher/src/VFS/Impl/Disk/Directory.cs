15e742afd837b837e4f681d075a290d98c3911de
Ash <ashe.null@protonmail.ch>
2021-01-31 15:33:14 -0600

    add disk VFS refreshing

--- Deli.Patcher/src/VFS/Impl/Disk/Directory.cs
+++ Deli.Patcher/src/VFS/Impl/Disk/Directory.cs
@@ -1,15 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
 namespace Deli.VFS.Disk
 {
-	public abstract class DirectoryHandle : IDirectoryHandle, IDiskHandle, IEnumerable<IDiskChildHandle>
+	public abstract class DirectoryHandle : IDirectoryHandle, IEnumerable<IDiskChildHandle>, IDiskHandle
 	{
 		private readonly Dictionary<string, IDiskChildHandle> _handles = new();
 
 		public string Path { get; }
-
 		public string PathOnDisk { get; }
 
 		protected DirectoryHandle(string path, string pathOnDisk)
@@ -17,36 +17,92 @@ namespace Deli.VFS.Disk
 			Path = path;
 			PathOnDisk = pathOnDisk;
 
-			Refresh();
+			AddNew(Directory.GetDirectories(PathOnDisk), Directory.GetFiles(PathOnDisk), _handles);
 		}
 
-		protected void Refresh()
+		// Adds handles that still exist on disk.
+		private void AddAlive(HashSet<string> directories, HashSet<string> files, Dictionary<string, IDiskChildHandle> buffer)
 		{
-			// TODO: flush out implementation (deleted files + updated files)
-
-			foreach (var path in Directory.GetFiles(PathOnDisk))
+			static bool IsAlive<THandle>(HashSet<string> paths, THandle handle) where THandle : IDiskHandle
 			{
-				var name = System.IO.Path.GetFileName(path);
-				var handle = new FileHandle(name, path, this);
+				var path = handle.PathOnDisk;
+				if (paths.Contains(path))
+				{
+					paths.Remove(path);
+					return true;
+				}
 
-				_handles.Add(handle.Name, handle);
+				return false;
 			}
 
-			foreach (var path in Directory.GetDirectories(PathOnDisk))
+			foreach (var entry in _handles)
+			{
+				var name = entry.Key;
+				var handle = entry.Value;
+
+				if (handle switch
+				{
+					DirectoryHandle directory => IsAlive(directories, directory),
+					FileHandle file => IsAlive(files, file),
+					_ => throw new InvalidOperationException("A non-Deli handle implementation got into the Deli directory implementation somehow.")
+				}) buffer.Add(name, handle);
+			}
+		}
+
+		// Add handles that have newly appeared on disk
+		private void AddNew(IEnumerable<string> directories, IEnumerable<string> files, Dictionary<string, IDiskChildHandle> buffer)
+		{
+			foreach (var path in directories)
 			{
 				var name = System.IO.Path.GetFileName(path);
 				var handle = new ChildDirectoryHandle(name, path, this);
 
-				_handles.Add(handle.Name, handle);
+				buffer.Add(handle.Name, handle);
+			}
+
+			foreach (var path in files)
+			{
+				var name = System.IO.Path.GetFileName(path);
+				var handle = new FileHandle(name, path, this);
+
+				buffer.Add(handle.Name, handle);
 			}
 		}
 
-		public IDiskChildHandle? this[string name] => _handles.TryGetValue(name, out var child) ? child : null;
+		public void Refresh()
+		{
+			this.ThrowIfDead();
+
+			var directories = new HashSet<string>(Directory.GetDirectories(PathOnDisk));
+			var files = new HashSet<string>(Directory.GetFiles(PathOnDisk));
+			var buffer = new Dictionary<string, IDiskChildHandle>();
+
+			AddAlive(directories, files, buffer);
+
+			foreach (var existing in buffer.Values)
+			{
+				existing.Refresh();
+			}
+
+			AddNew(directories, files, buffer);
+		}
+
+		public IDiskChildHandle? this[string name]
+		{
+			get
+			{
+				this.ThrowIfDead();
+
+				return _handles.TryGetValue(name, out var child) ? child : null;
+			}
+		}
 
 		IChildHandle? IDirectoryHandle.this[string name] => this[name];
 
 		public Dictionary<string, IDiskChildHandle>.ValueCollection.Enumerator GetEnumerator()
 		{
+			this.ThrowIfDead();
+
 			return _handles.Values.GetEnumerator();
 		}
 
