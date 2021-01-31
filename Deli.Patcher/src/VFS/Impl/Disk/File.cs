ec87ae95398fabc425304e6636807934dd1c80d9
Ash <ashe.null@protonmail.ch>
2021-01-31 00:12:26 -0600

    remove uncompliant mono method

--- Deli.Patcher/src/VFS/Impl/Disk/File.cs
+++ Deli.Patcher/src/VFS/Impl/Disk/File.cs
@@ -26,7 +26,7 @@ namespace Deli.VFS.Disk
 
 		public Stream OpenRead()
 		{
-			return new FileStream(PathOnDisk, FileMode.Open, FileAccess.Read, FileShare.Read, 64 * 1024, FileOptions.Asynchronous);
+			return new FileStream(PathOnDisk, FileMode.Open, FileAccess.Read, FileShare.Read, 64 * 1024, true);
 		}
 
 		public override string ToString()
